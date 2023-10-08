94480a7d442af917424a87043a34160da8f8a0e8
Josué Caleb <josshuacaleb@gmail.com>
2023-10-08 02:45:20 -0300

    Rewrite to use hook_safe; Add two new sfx on peril explosion; Fix settings bug;

---
+++ README.md
@@ -0,0 +1,40 @@
+# Unlimited Power
+## Description
+This is a simple mod that plays various Palpatine related SFX when certain game actions happen. It's a bit of a meme mod, but I hope you enjoy it nonetheless.
+
+## Features
+### Audio
+* Plays "Unlimited power" on Blitz Smite charge
+* On force sword special activation, plays "It's treason then"
+* When peril has reached 100 and you have your hands up, plays "No, no, no!"
+* When peril has reached 100 and you blow up, plays "The attempt on my life..."
+
+### UI
+Mod options are available to set the volume, frequency of the audio and more.
+
+## Installation
+1. Install dependencies:
+    1. [Darktide Modding Framework](https://www.nexusmods.com/warhammer40kdarktide/mods/8)
+    2. [Darktide Mod Loader](https://www.nexusmods.com/warhammer40kdarktide/mods/19)
+    3. [Darktide Local Server](https://www.nexusmods.com/warhammer40kdarktide/mods/211)
+    4. [Audio Plugin](https://www.nexusmods.com/warhammer40kdarktide/mods/196)
+2. Download the latest version of the mod from the [releases page](https://github.com/JCalebBR/UnlimitedPower/releases/latest).
+3. Extract the `UnlimitedPower` folder into your `mods` folder.
+![](images/image.png)
+4. Place it in your load order. Should be as follows:
+    * DarktideLocalServer
+    * Audio
+    * UnlimitedPower
+5. ???
+6. Profit
+
+## Limitations
+* You need Show Developer Console set to ON and Developer Mode set to ON in DMF settings as per screenshot below, otherwise you'll get a popup cmd window every time the audio file plays. Thanks to **raretrick** for reporting the issue and **SeventeenV** for the graphic.
+* This is a limitation from Darktide Local Server, nothing I can do about it for now.
+![](images/image-1.png)
+
+## Credits
+
+Thanks to DMF, DML and DLS devs for making this possible.
+Thanks to my ADHD for making me do this.
+I do not own the sound files used in this mod, they are from the Star Wars franchise.
--- scripts/mods/UnlimitedPower/UnlimitedPower.lua
+++ scripts/mods/UnlimitedPower/UnlimitedPower.lua
@@ -1,9 +1,8 @@
-local settings_volume = 50
-local settings_chance = 10
-local settings_shadow = false
-
 local mod = get_mod("UnlimitedPower")
-local Audio
+
+local settings_volume = mod:get("mod_volume")
+local settings_chance = mod:get("mod_chance")
+local settings_shadow = mod:get("mod_shadow")
 mod.on_setting_changed = function(setting_name)
     if setting_name == "mod_volume" then
         settings_volume = mod:get(setting_name)
@@ -14,8 +13,10 @@ mod.on_setting_changed = function(setting_name)
     end
 end
 
-mod.on_all_mods_loaded = function()
-    LocalServer = get_mod("DarktideLocalServer")
+local LocalServer = get_mod("DarktideLocalServer")
+local Audio
+local check_dependencies = function()
+    Audio = get_mod("Audio")
     if not LocalServer then
         mod:echo(
             'Required mod "Darktide Local Server" not found: Download from Nexus Mods and include in mod_load_order.txt'
@@ -23,53 +24,61 @@ mod.on_all_mods_loaded = function()
         mod:disable_all_hooks()
         mod:disable_all_commands()
     end
-    Audio = get_mod("Audio")
-    if not LocalServer then
+    if not Audio then
         mod:echo(
             'Required mod "Audio Plugin" not found: Download from Nexus Mods and include in mod_load_order.txt'
         )
         mod:disable_all_hooks()
         mod:disable_all_commands()
     end
-    Audio.hook_sound("play_psyker_lightning_bolt_charge", function(sound_type, sound_name, delta)
-        if delta == nil or delta > 6 then
-            if math.random() < (settings_chance / 100) then
-                if settings_shadow == true then
-                    Audio.play_file("UnlimitedPower/audio/shadow.ogg",
-                        { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
-                else
-                    Audio.play_file("UnlimitedPower/audio/unlimitedpower.ogg",
-                        { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
+end
+
+local action_chain_lightning_new_last = 0
+local ActionActivateSpecial_last = 0
+mod.on_all_mods_loaded = function()
+    check_dependencies()
+    mod:hook_require("scripts/extension_systems/weapon/actions/action_chain_lightning_new", function(instance)
+        mod:hook_safe(instance, "start", function(self, action_settings, t, time_scale, action_start_params)
+            if action_chain_lightning_new_last + 6 < t then
+                if math.random() < (settings_chance / 100) then
+                    if settings_shadow == true then
+                        Audio.play_file("UnlimitedPower/audio/shadow.ogg",
+                            { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
+                    else
+                        Audio.play_file("UnlimitedPower/audio/unlimitedpower.ogg",
+                            { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
+                    end
+                    action_chain_lightning_new_last = t
                 end
             end
-        end
-        return true
+        end)
     end)
-    Audio.hook_sound("play_force_sword_on", function(sound_type, sound_name, delta)
-        if delta == nil or delta > 3 then
-            if math.random() < (settings_chance / 100) then
-                Audio.play_file("UnlimitedPower/audio/itstreasonthen.ogg",
-                    { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
-            end
-        end
-        return true
-    end)
-    Audio.hook_sound("play_warp_charge_build_up_critical", function(sound_type, sound_name, delta)
-        if delta == nil or delta > 4 then
+
+    mod:hook_require("scripts/extension_systems/weapon/actions/action_overload_explosion", function(instance)
+        mod:hook_safe(instance, "start", function()
             if math.random() < (settings_chance / 100) then
                 Audio.play_file("UnlimitedPower/audio/nonono.ogg",
                     { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
             end
-        end
-        return true
-    end)
-    Audio.hook_sound("play_psyker_overload_charge_start", function(sound_type, sound_name, delta)
-        if delta == nil or delta > 4 then
+        end)
+        mod:hook_safe(instance, "finish", function()
             if math.random() < (settings_chance / 100) then
                 Audio.play_file("UnlimitedPower/audio/theattemptonmylife.ogg",
                     { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
             end
+        end)
+    end)
+
+    mod:hook_safe(CLASS.ActionActivateSpecial, "start", function(self, action_settings, t, time_scale, params)
+        mod:echo(table.contains(self._weapon_template.keywords, "force_sword"))
+        if ActionActivateSpecial_last + 3 < t then
+            if math.random() < (settings_chance / 100) then
+                if (table.contains(self._weapon_template.keywords, "force_sword") == true) then
+                    Audio.play_file("UnlimitedPower/audio/itstreasonthen.ogg",
+                        { audio_type = "sfx", volume = settings_volume, speechnorm = "e=50:r=0.0001:l=1" })
+                end
+                ActionActivateSpecial_last = t
+            end
         end
-        return true
     end)
 end
