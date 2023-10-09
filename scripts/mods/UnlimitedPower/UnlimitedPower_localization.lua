9edfd35433a31e9ccac0211497025e2502f3c288
xsSplater <xssplater@gmail.com>
2023-10-09 05:05:30 +0300

    RU UnlimitedPower_localization.lua
    +mod name

--- scripts/mods/UnlimitedPower/UnlimitedPower_localization.lua
+++ scripts/mods/UnlimitedPower/UnlimitedPower_localization.lua
@@ -1,24 +1,35 @@
 return {
+	mod_name = {
+		en = "Unlimited Power",
+		ru = "Абсолютная ВлАААААААсть!",
+	},
 	mod_description = {
 		en = "Unlimited Power. That's it. It plays the meme sound when you use the Smite Blitz and other memey sfx.",
+		ru = "Unlimited Power - Вот и всё. Мод воспроизводит звук мема, когда вы используете «Сокрушение» и другие мемные эффекты.",
 	},
 	mod_volume_title = {
 		en = "Volume",
+		ru = "Громкость",
 	},
 	mod_volume_description = {
 		en = "Sets the volume for all sounds, please be gentle to your ears.",
+		ru = "Установите громкость всех звуков, будьте осторожны со своими ушами.",
 	},
 	mod_chance_title = {
 		en = "SFX Chance",
+		ru = "Шанс срабатывания",
 	},
 	mod_chance_description = {
 		en =
 		"Sets the chance for the sound to play, in percent. You don't want the same sound to play all the time, right??",
+		ru = "Устанавливает вероятность воспроизведения звука в процентах. Вы же не хотите, чтобы один и тот же звук воспроизводился все время, верно?",
 	},
 	mod_shadow_title = {
 		en = "Shadow Wizard Money Gang",
+		ru = "Shadow Wizard Money Gang",
 	},
 	mod_shadow_description = {
 		en = "Plays the \"Shadow Wizard Money Gang\" sfx instead of the \"Unlimited Power\" sfx.",
+		ru = "Воспроизводит звуковой эффект «Shadow Wizard Money Gang» вместо звукового эффекта «Unlimited Power».",
 	},
 }
